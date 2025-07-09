91606100a7fd494635075d9464918204a3840753
Victor Juwa <juwavictor@gmail.com>
2025-07-09 17:51:22 +0100

    chore: setup vite configuration and eslint for server

--- html-version/vite.config.js
+++ html-version/vite.config.js
@@ -1,6 +1,43 @@
-module.exports = {
-  root: 'src',
+import { resolve } from 'path';
+import { defineConfig } from 'vite';
+import eslint from 'vite-plugin-eslint';
+
+export default defineConfig({
+  publicDir: 'public',
+  root: './',
   build: {
-    outDir: '../dist'
-  }
-}
+    outDir: 'dist',
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+      },
+      output: {
+        manualChunks(id) {
+          if (id.includes('node_modules')) {
+            const modulePath = id.split('node_modules/')[1];
+
+            const topLevelFolder = modulePath?.split('/')[0];
+
+            if (!topLevelFolder) {
+              const scopedPackageName = modulePath?.split('/')[1];
+              return scopedPackageName?.split('@')[
+                scopedPackageName.startsWith('@') ? 1 : 0
+              ];
+            }
+
+            return topLevelFolder;
+          }
+
+          return null;
+        },
+      },
+    },
+    minify: 'esbuild',
+  },
+  plugins: [
+    eslint({
+      cache: false,
+      fix: true,
+    }),
+  ],
+});
